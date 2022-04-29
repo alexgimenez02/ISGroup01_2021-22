6a342e9d7b665ce99b49c671ca6abf2ac66a1b4f
andreucastano01 <andreu.castano01@estudiant.upf.edu>
2022-04-29 11:56:07 +0200

    Platypus base + playerController

---
+++ Clean-lab-proyect-main/Assets/scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 playerPos, lastPosition;
+    private float waitTime;
+    private bool holdPiece;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPos = transform.position;
+        lastPosition = playerPos;
+        waitTime = 0.0f;
+        holdPiece = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO
+        /*
+         1. Tiempo de espera cuando esta quieto
+         2. Mover pieza
+         3. Generar escudo
+         4. Dejar pieza
+         
+         
+        */
+        playerPos = transform.position;
+
+        if(waitTime >= 3.0f && !holdPiece)
+        {
+            //Show options
+            //if(player on top of piece) 1. grab option 2. shield option
+            //else shield option
+
+            Debug.Log("Hold or shield piece");
+            holdPiece = true;
+            waitTime = 0.0f;
+        }
+        else if(waitTime >= 3.0f && holdPiece)
+        {
+            //Drop piece
+
+            Debug.Log("Drop piece");
+            waitTime = 0.0f;
+            holdPiece = false;
+        }
+
+    }
+    void LateUpdate()
+    {
+        
+        if (playerPos == lastPosition)
+        {
+            waitTime += Time.deltaTime;
+        }
+        else
+        {
+            waitTime = 0.0f;
+        }
+        lastPosition = playerPos;
+    }
+
+    
+}
