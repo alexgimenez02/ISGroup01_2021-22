51359404ed470c9ebe089ba4452edef1ce8a0af1
alexgimenez02 <alex.gimenez02@estudiant.upf.edu>
2022-05-04 23:04:56 +0200

    Se mueven las piezas!
    Not yet working, need to be fixed with a distance matrix or array

--- Clean-lab-proyect-main/Assets/scripts/PlayerController.cs
+++ Clean-lab-proyect-main/Assets/scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     private float waitTime;
     private bool holdPiece;
     private float fixTime = 2.0f;
+    public string color;
+    private GameObject currentPiece;
 
 
     // Start is called before the first frame update
@@ -17,6 +19,7 @@ public class PlayerController : MonoBehaviour
         lastPosition = playerPos;
         waitTime = 0.0f;
         holdPiece = false;
+       
     }
 
     // Update is called once per frame
@@ -33,14 +36,62 @@ public class PlayerController : MonoBehaviour
         */
         playerPos = transform.position;
 
+        if(holdPiece && currentPiece != null)
+        {
+            Debug.Log("Moving piece");
+            currentPiece.transform.position = playerPos + new Vector3(0.0f,2.0f,0.0f);
+        }
+
         if(waitTime >= fixTime && !holdPiece)
         {
             //Show options
             //if(player on top of piece) 1. grab option 2. shield option
+            for(int i = 0; i < 7; ++i)
+            {
+                float distToPiece;
+                string name = "piece" + (i + 1);
+                GameObject piece = GameObject.Find(name); 
+                
+                if(piece.gameObject.GetComponent<MeshRenderer>()) //Triangulos
+                {
+                    
+                    //Debug.Log("Piece: " + piece.gameObject.GetComponent<MeshRenderer>().material.ToString());
+                    //Debug.Log("Player: " + color + "(Instance) (UnityEngine.Material)");
+                    
+                    if(piece.gameObject.GetComponent<MeshRenderer>().material.ToString() == (color + " (Instance) (UnityEngine.Material)"))
+                    {
+                        distToPiece = Vector3.Distance(piece.gameObject.transform.position,playerPos);
+                        //Debug.Log("Player distance to piece " + (i+1) + distToPiece);
+                        if(distToPiece < 42)
+                        {
+                            holdPiece = true; //Debug.Log("Hold piece"); 
+                            currentPiece = piece;
+                            break;
+                        }
+                    }
+                }else 
+                {
+                    if(piece.gameObject.GetComponent<SpriteRenderer>()) //Rombos
+                    {
+                        //Material name when running -> color (Instance) (UnityEngine.Material)
+                        if(piece.gameObject.GetComponent<SpriteRenderer>().material.ToString() == (color + " (Instance) (UnityEngine.Material)"))
+                        {
+                            //Debug.Log("Same color: " + color);
+                            distToPiece = Vector3.Distance(piece.gameObject.transform.position,playerPos);
+                            if(distToPiece < 42)
+                            {
+                                holdPiece = true; //Debug.Log("Hold piece"); 
+                                currentPiece = piece;
+                                break;
+                            }
+                        }
+                    }
+                }
+            
+            }
             //else shield option
 
-            Debug.Log("Hold or shield piece");
-            holdPiece = true;
+            //Debug.Log("Hold or shield piece");
             waitTime = 0.0f;
         }
         else if(waitTime >= fixTime && holdPiece)
@@ -51,7 +102,10 @@ public class PlayerController : MonoBehaviour
             waitTime = 0.0f;
             holdPiece = false;
         }
-
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            holdPiece = false;
+        }
     }
 
     void LateUpdate()
