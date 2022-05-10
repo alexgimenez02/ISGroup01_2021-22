5c877b0eba9673fbaeb5892a4879e298d12b3a72
roroco7 <77933100+roroco7@users.noreply.github.com>
2022-05-10 12:41:20 +0200

    rombos y bandera lgtb incorporados

--- Clean-lab-proyect-main/Assets/scripts/PlayerController.cs
+++ Clean-lab-proyect-main/Assets/scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     private float waitTime;
     private bool holdPiece;
     private float fixTime = 2.0f;
+    public string color;
+    private GameObject currentPiece;
+    private Dictionary<string,float> redPieces = new Dictionary<string,float>();
 
 
     // Start is called before the first frame update
@@ -17,6 +20,27 @@ public class PlayerController : MonoBehaviour
         lastPosition = playerPos;
         waitTime = 0.0f;
         holdPiece = false;
+        for(int i = 0; i < 7; i++)
+        {
+            string name = "piece" + (i + 1);
+            GameObject piece = GameObject.Find(name); 
+            if(piece.gameObject.GetComponent<MeshRenderer>()) //Triangulos
+            {                 
+                if(piece.gameObject.GetComponent<MeshRenderer>().material.ToString() == (color + " (Instance) (UnityEngine.Material)"))
+                {
+                    redPieces.Add(name,Vector3.Distance(piece.gameObject.transform.position,playerPos));
+                }
+            }
+            else{
+                if(piece.gameObject.GetComponent<SpriteRenderer>()) //Rombos
+                {
+                    if(piece.gameObject.GetComponent<SpriteRenderer>().material.ToString() == (color + " (Instance) (UnityEngine.Material)"))
+                    {
+                        redPieces.Add(name,Vector3.Distance(piece.gameObject.transform.position,playerPos));
+                    }
+                }
+            }
+        }
     }
 
     // Update is called once per frame
@@ -33,14 +57,31 @@ public class PlayerController : MonoBehaviour
         */
         playerPos = transform.position;
 
+        if(holdPiece && currentPiece != null)
+        {
+            Debug.Log("Moving piece");
+            currentPiece.transform.position = playerPos + new Vector3(0.0f,2.0f,0.0f);
+        }
+
         if(waitTime >= fixTime && !holdPiece)
         {
-            //Show options
-            //if(player on top of piece) 1. grab option 2. shield option
-            //else shield option
+            float minDist = 200000000000;
+            string closePiece = "";
+            foreach (string key in redPieces.Keys)
+            {
+                minDist = Mathf.Min(minDist,redPieces[key]);
+                if(minDist == redPieces[key]) closePiece = key;
+            }
 
-            Debug.Log("Hold or shield piece");
-            holdPiece = true;
+            if(closePiece != "" && redPieces[closePiece] < 42)
+            {
+                GameObject piece = GameObject.Find(closePiece);
+                currentPiece = piece;
+                holdPiece = true;
+                Debug.Log("Hold piece!");
+            }
+
+            //Debug.Log("Hold or shield piece");
             waitTime = 0.0f;
         }
         else if(waitTime >= fixTime && holdPiece)
@@ -50,8 +91,24 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Drop piece");
             waitTime = 0.0f;
             holdPiece = false;
+            currentPiece.transform.position = playerPos + new Vector3(0.0f,40.0f,0.0f);
         }
-
+        if(!holdPiece)
+        {
+            List<string> keys = new List<string>(redPieces.Keys);
+            foreach(string key in keys)
+            {
+                GameObject piece = GameObject.Find(key);
+                redPieces[key] = Vector3.Distance(piece.gameObject.transform.position,playerPos);
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            holdPiece = false;
+            Debug.Log("Debug Piece Drop!");
+        }
+        if(Input.GetKeyDown(KeyCode.T))
+            Debug.Log("WaitTime is: " + waitTime);
     }
 
     void LateUpdate()
